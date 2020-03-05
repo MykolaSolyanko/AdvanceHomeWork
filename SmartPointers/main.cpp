748501daa396ab02817607e77722c95c39469c93
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2020-03-05 18:01:21 +0200

    Google code style

--- SmartPointers/main.cpp
+++ SmartPointers/main.cpp
@@ -4,12 +4,11 @@
 struct X {};
 struct Y : public X {};
 
-int main()
-{
-	Unique_Ptr<X>   x(new X);
-	Unique_Ptr<Y>   y(new Y);
+int main() {
+  Unique_Ptr<X> x(new X);
+  Unique_Ptr<Y> y(new Y);
 
-	x = std::move(y);
-	Unique_Ptr<X>   z(std::move(y));
-	Unique_Ptr<X> obj(nullptr);
+  x = std::move(y);
+  Unique_Ptr<X> z(std::move(y));
+  Unique_Ptr<X> obj(nullptr);
 }
