ba840f31569828ba1158fe8c6cca9d87edf9e19b
RevychMaksym <55156318+RevychMaksym@users.noreply.github.com>
2020-03-05 18:59:25 +0300

    Unique_Pointer_Implementation

---
+++ SmartPointers/main.cpp
@@ -0,0 +1,15 @@
+#include "Unique_Ptr.hpp"
+#include <utility>
+
+struct X {};
+struct Y : public X {};
+
+int main()
+{
+	Unique_Ptr<X>   x(new X);
+	Unique_Ptr<Y>   y(new Y);
+
+	x = std::move(y);
+	Unique_Ptr<X>   z(std::move(y));
+	Unique_Ptr<X> obj(nullptr);
+}
